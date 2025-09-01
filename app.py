67d579919809746598b8abd4999cd83d1ddbcae7
SebaCape <scapellan55@gmail.com>
2025-08-31 23:56:25 -0400

    Switched to gunicorn port for deployment

--- app.py
+++ app.py
@@ -4,13 +4,6 @@ import json
 
 app = Flask(__name__)
 
-import os
-
-# Port definition for Render
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
-
 @app.route("/")
 def home():
     return render_template("index.html")
